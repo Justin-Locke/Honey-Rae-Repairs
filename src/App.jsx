ad58f1862be82050eabf919c8ef98046772f3678
Justin Locke <Justindeanlocke@gmail.com>
2025-02-16 21:50:59 -0600

    Able to render Employees now too.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import "./app.css"
 import { CustomersList } from "./components/customers/CustomersList"
+import { EmployeesList } from "./components/employees/EmployeesList"
 import { TicketList } from "./components/tickets/TicketList"
 
 export const App = () => {
   return <>
     {/* <TicketList /> */}
-    <CustomersList />
+    {/* <CustomersList /> */}
+    <EmployeesList />
   </>
 }
