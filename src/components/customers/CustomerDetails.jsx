5bd27faf1773932b4be06940ea646e78124377d8
Justin Locke <Justindeanlocke@gmail.com>
2025-02-19 12:38:22 -0600

    Made changes to show single customer. Changed params to look for userId instead of customerId or employeeId.

--- src/components/customers/CustomerDetails.jsx
+++ src/components/customers/CustomerDetails.jsx
@@ -1,8 +1,39 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { getCustomerByUserId } from "../../services/customerService"
+import "./Customers.css"
 
 export const CustomerDetails = () => {
-    
-    const { customerId } = useParams()
+    const [customer, setCustomer] = useState({})
+    const { userId } = useParams()
 
-    return <div>Customer #: {customerId}</div>
+
+    useEffect(() => {
+        getCustomerByUserId(userId).then(data => {
+            const customerObj = data[0]
+            setCustomer(customerObj)
+        })
+    }, [userId]) 
+
+
+
+    return (
+    <section className="customer">
+        <header className="customer-header">
+            {customer.user?.fullName}
+        </header>
+        <div>
+            <span className="customer-info">Email : </span>
+            {customer.user?.email}
+        </div>
+        <div>
+            <span className="customer-info">Address : </span>
+            {customer.address}
+        </div>
+        <div>
+            <span className="customer-info">Phone Number :</span>
+            {customer.phoneNumber}
+        </div>
+    </section>
+    )
 }
